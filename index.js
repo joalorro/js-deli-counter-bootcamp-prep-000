de7a522d5e5e682d32202609ea1394ff20c3bffe
Josue V. Alorro <joalorro@gmail.com>
2018-06-20 14:30:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ function nowServing (katzDeliLine) {
 
 function currentLine(line) {
   
-  var msg = "The line is currently: ";
+  
   if (line.length === 0) {
     
     return "The line is currently empty.";
     
   } else {
-    
+    var msg = "The line is currently: ";
     for (let i = 0; i < line.length - 1; i++) {
       
       msg += `${i + 1}. ${line[i]}, `;
